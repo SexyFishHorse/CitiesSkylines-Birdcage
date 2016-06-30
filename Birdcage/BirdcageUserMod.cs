1018342439851c6434c152e4974c892377794c49
Asbjørn Hansen <ah@fenris.io>
2016-06-30 09:03:14 +0200

    added user mod class for birdcage

---
+++ Birdcage/BirdcageUserMod.cs
@@ -0,0 +1,24 @@
+﻿namespace SexyFishHorse.CitiesSkylines.Birdcage
+{
+    using System;
+    using ICities;
+    using SexyFishHorse.CitiesSkylines.Infrastructure;
+
+    public class BirdcageUserMod : IUserModWithOptionsPanel
+    {
+        public string Name
+        {
+            get { return "Birdcage"; }
+        }
+
+        public string Description
+        {
+            get { return "More Chirper controls"; }
+        }
+
+        public void OnSettingsUI(UIHelperBase uiHelper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
