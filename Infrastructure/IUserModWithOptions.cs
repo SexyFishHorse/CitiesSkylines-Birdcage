3d6c9bcd8a876120db7eb32f30b5ba4590d94d39
Asbjørn Hansen <ah@fenris.io>
2016-06-30 09:02:47 +0200

    added interface for mod with options panel

---
+++ Infrastructure/IUserModWithOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace SexyFishHorse.CitiesSkylines.Infrastructure
+{
+    using ICities;
+    using JetBrains.Annotations;
+
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    public interface IUserModWithOptionsPanel : IUserMod
+    {
+        // ReSharper disable once InconsistentNaming
+        void OnSettingsUI(UIHelperBase uiHelper);
+    }
+}
