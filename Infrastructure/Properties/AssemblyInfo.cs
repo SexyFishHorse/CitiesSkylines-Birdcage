e0d2467138cac0694bf38757943dce9330360da8
Asbjørn Hansen <ah@fenris.io>
2016-06-30 09:01:33 +0200

    created new solution and projects

---
+++ Infrastructure/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("SexyFishHorse.CitiesSkylines.Infrastructure")]
+[assembly: AssemblyDescription("Infrastructure classes and code for Cities Skylines modding")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SexyFishHorse")]
+[assembly: AssemblyProduct("SexyFishHorse.CitiesSkylines.Infrastructure")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("a5a13615-047f-4eaa-8379-4eddb2e6a982")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
