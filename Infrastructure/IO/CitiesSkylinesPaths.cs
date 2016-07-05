51985b0888a8daeca1ebf3d71f7129f94c49ea5a
Asbjørn Hansen <ah@fenris.io>
2016-07-05 21:01:36 +0200

    added helper class for getting mod folder names

---
+++ Infrastructure/IO/CitiesSkylinesPaths.cs
@@ -0,0 +1,26 @@
+﻿namespace SexyFishHorse.CitiesSkylines.Infrastructure.IO
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using JetBrains.Annotations;
+
+    public static class CitiesSkylinesPaths
+    {
+        public static string GetModFolderPath([NotNull] string modFolderName, ModFolder modFolder)
+        {
+            var localAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var colossalOrderPath = Path.Combine(localAppDataPath, "Colossal Order");
+            var citiesSkylinesPath = Path.Combine(colossalOrderPath, "Cities_Skylines");
+            var modFolderPath = Path.Combine(citiesSkylinesPath, modFolderName);
+
+            var attribute =
+                modFolder.GetType()
+                         .GetCustomAttributes(typeof(ModFolderPathAttribute), false)
+                         .Cast<ModFolderPathAttribute>()
+                         .Single();
+
+            return attribute.Paths.Aggregate(modFolderPath, Path.Combine);
+        }
+    }
+}
