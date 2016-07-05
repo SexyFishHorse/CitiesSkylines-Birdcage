6523607f2270249bb56dc323a75e97bdd6739d5a
Asbjørn Hansen <ah@fenris.io>
2016-07-05 21:06:08 +0200

    added initial config store for config changes

---
+++ Infrastructure/Configuration/ConfigStore.cs
@@ -0,0 +1,18 @@
+﻿namespace SexyFishHorse.CitiesSkylines.Infrastructure.Configuration
+{
+    using System;
+    using System.Collections;
+
+    public class ConfigStore
+    {
+        public T GetSetting<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveSetting<T>(string key, T value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
