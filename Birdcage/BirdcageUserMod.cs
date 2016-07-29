a93e322df7532e3b7e5c971c403b2783c3e49faf
Asbjørn Hansen <ah@fenris.io>
2016-07-29 05:03:38 +0200

    made chirper draggable

--- Birdcage/BirdcageUserMod.cs
+++ Birdcage/BirdcageUserMod.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using ColossalFramework.Plugins;
+    using ColossalFramework.UI;
     using ICities;
     using SexyFishHorse.CitiesSkylines.Infrastructure;
     using SexyFishHorse.CitiesSkylines.Infrastructure.Configuration;
@@ -13,6 +14,14 @@
 
     public class BirdcageUserMod : ChirperExtensionBase, IUserModWithOptionsPanel
     {
+        private const string SettingKeysChirperAnchor = "ChirperAnchor";
+
+        private const string SettingKeysChirperPositionX = "ChirperPositionX";
+
+        private const string SettingKeysChirperPositionY = "ChirperPositionY";
+
+        private const string SettingKeysDraggable = "Draggable";
+
         private const string SettingKeysFilterMessages = "FilterMessages";
 
         private const string SettingKeysHideChirper = "HideChirper";
@@ -23,9 +32,19 @@
 
         private readonly HashSet<IChirperMessage> messagesToRemove;
 
+        private bool controlDown;
+
+        private bool draggable;
+
+        private bool dragging;
+
         private bool filterNonImportantMessages;
 
+        private bool mouseDown;
+
         private AudioClip notificationSound;
+
+        private UIView uiView;
 
         public BirdcageUserMod()
         {
@@ -35,6 +54,7 @@
             logger = LogManager.Instance.GetOrCreateLogger("Birdcage");
 
             filterNonImportantMessages = configStore.GetSetting<bool>(SettingKeysFilterMessages);
+            draggable = configStore.GetSetting<bool>(SettingKeysDraggable);
         }
 
         public string Description
@@ -57,10 +77,34 @@
         {
             base.OnCreated(c);
 
+            if (configStore.HasSetting(SettingKeysChirperPositionX))
+            {
+                var chirperX = configStore.GetSetting<int>(SettingKeysChirperPositionX);
+                var chirperY = configStore.GetSetting<int>(SettingKeysChirperPositionY);
+                var chirperPosition = new Vector2(chirperX, chirperY);
+                chirperPosition = EnsurePositionIsOnScreen(chirperPosition);
+
+                var chirperAnchor = (ChirperAnchor)configStore.GetSetting<int>(SettingKeysChirperAnchor);
+
+                chirper.builtinChirperPosition = chirperPosition;
+                chirper.SetBuiltinChirperAnchor(chirperAnchor);
+            }
+            else
+            {
+                chirper.builtinChirperPosition = new Vector2(100, 100);
+                chirper.SetBuiltinChirperAnchor(ChirperAnchor.TopLeft);
+            }
+
+            if (draggable)
+            {
+                c.SetBuiltinChirperFree(true);
+            }
+
             var hideChirper = configStore.GetSetting<bool>(SettingKeysHideChirper);
 
             notificationSound = ChirpPanel.instance.m_NotificationSound;
             ChirpPanel.instance.gameObject.SetActive(!hideChirper);
+            uiView = ChirpPanel.instance.component.GetUIView();
         }
 
         public override void OnNewMessage(IChirperMessage message)
@@ -90,10 +134,22 @@
             {
                 var helper = uiHelper.AsStronglyTyped();
 
-                helper.AddCheckBox("Hide chirper", configStore.GetSetting<bool>(SettingKeysHideChirper), ToggleChirper);
-                helper.AddCheckBox(
-                    "Filter non-important messages", 
-                    configStore.GetSetting<bool>(SettingKeysFilterMessages), 
+                var appearanceGroup = helper.AddGroup("Appearance");
+
+                appearanceGroup.AddCheckBox(
+                    "Hide chirper",
+                    configStore.GetSetting<bool>(SettingKeysHideChirper),
+                    ToggleChirper);
+                appearanceGroup.AddCheckBox(
+                    "Make Chirper draggable",
+                    configStore.GetSetting<bool>(SettingKeysDraggable),
+                    ToggleDraggable);
+
+                var behaviourGroup = helper.AddGroup("Behaviour");
+
+                behaviourGroup.AddCheckBox(
+                    "Filter non-important messages",
+                    configStore.GetSetting<bool>(SettingKeysFilterMessages),
                     ToggleFilter);
             }
             catch (Exception ex)
@@ -106,6 +162,11 @@
         {
             try
             {
+                if (ChirpPanel.instance == null)
+                {
+                    return;
+                }
+
                 if (messagesToRemove.Any())
                 {
                     ChirpPanel.instance.Collapse();
@@ -118,11 +179,134 @@
                     ChirpPanel.instance.m_NotificationSound = notificationSound;
                     messagesToRemove.Clear();
                 }
+
+                if (draggable)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        mouseDown = true;
+                    }
+
+                    if (Input.GetMouseButtonUp(0))
+                    {
+                        mouseDown = false;
+                    }
+
+                    if (dragging && mouseDown == false)
+                    {
+                        dragging = false;
+                        SaveChirperPositionAndUpdateAnchor();
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl))
+                    {
+                        controlDown = true;
+                    }
+
+                    if ((Input.GetKeyUp(KeyCode.LeftControl) && !Input.GetKey(KeyCode.RightControl))
+                        || (Input.GetKeyUp(KeyCode.RightControl) && !Input.GetKey(KeyCode.LeftCommand)))
+                    {
+                        controlDown = false;
+                    }
+
+                    if (!dragging && mouseDown && controlDown)
+                    {
+                        if (IsMouseOnChirper())
+                        {
+                            dragging = true;
+                        }
+                    }
+
+                    if (dragging)
+                    {
+                        var mousePosition = GetMouseGuiPosition();
+
+                        mousePosition = EnsurePositionIsOnScreen(mousePosition);
+
+                        chirper.builtinChirperPosition = mousePosition;
+                    }
+                }
             }
             catch (Exception ex)
             {
                 logger.LogException(ex, PluginManager.MessageType.Error);
             }
+        }
+
+        private Vector2 EnsurePositionIsOnScreen(Vector2 mousePosition)
+        {
+            if (mousePosition.x < 0)
+            {
+                mousePosition.x = 0;
+            }
+
+            if (mousePosition.x > uiView.GetScreenResolution().x)
+            {
+                mousePosition.x = uiView.GetScreenResolution().x;
+            }
+
+            if (mousePosition.y < 0)
+            {
+                mousePosition.y = 0;
+            }
+
+            if (mousePosition.y > uiView.GetScreenResolution().y)
+            {
+                mousePosition.y = uiView.GetScreenResolution().y;
+            }
+            return mousePosition;
+        }
+
+        private Vector2 GetMouseGuiPosition()
+        {
+            var mouseWorldPos = Camera.current.ScreenToWorldPoint(Input.mousePosition);
+            var mouseGuiPos = uiView.WorldPointToGUI(uiView.uiCamera, mouseWorldPos);
+            return mouseGuiPos;
+        }
+
+        private bool IsMouseOnChirper()
+        {
+            var mouseGuiPos = GetMouseGuiPosition();
+
+            var pointAndChirperMagnitude = mouseGuiPos - chirper.builtinChirperPosition;
+
+            var mouseOnChirper = pointAndChirperMagnitude.magnitude < 35;
+            return mouseOnChirper;
+        }
+
+        private void SaveChirperPositionAndUpdateAnchor()
+        {
+            var thirdOfUiWidth = uiView.GetScreenResolution().x / 3;
+            var halfUiHeight = uiView.GetScreenResolution().y / 2;
+
+            ChirperAnchor anchor;
+
+            if (chirper.builtinChirperPosition.x < thirdOfUiWidth)
+            {
+                anchor = chirper.builtinChirperPosition.y < halfUiHeight
+                             ? ChirperAnchor.TopLeft
+                             : ChirperAnchor.BottomLeft;
+            }
+            else if (chirper.builtinChirperPosition.x > thirdOfUiWidth * 2)
+            {
+                anchor = chirper.builtinChirperPosition.y < halfUiHeight
+                             ? ChirperAnchor.TopRight
+                             : ChirperAnchor.BottomRight;
+            }
+            else if (chirper.builtinChirperPosition.y < halfUiHeight)
+            {
+                anchor = ChirperAnchor.TopCenter;
+            }
+            else
+            {
+                anchor = ChirperAnchor.BottomCenter;
+            }
+
+            chirper.SetBuiltinChirperAnchor(anchor);
+
+            configStore.SaveSetting(SettingKeysChirperPositionX, (int)chirper.builtinChirperPosition.x);
+            configStore.SaveSetting(SettingKeysChirperPositionY, (int)chirper.builtinChirperPosition.y);
+            configStore.SaveSetting(SettingKeysChirperAnchor, (int)anchor);
         }
 
         private bool ShouldFilterMessage(CitizenMessage citizenMessage)
@@ -170,6 +354,15 @@
             catch (Exception ex)
             {
                 logger.LogException(ex, PluginManager.MessageType.Error);
+            }
+        }
+
+        private void ToggleDraggable(bool isDraggable)
+        {
+            draggable = isDraggable;
+            if (chirper != null)
+            {
+                chirper.SetBuiltinChirperFree(true);
             }
         }
 
